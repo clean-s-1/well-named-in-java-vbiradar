0069e3f721b0a994e3168c4faef1d826563c6bb8
vbiradar <vikhyat.biradar@gmail.com>
2022-05-24 13:08:12 +0530

    removed common identical method

--- colorcoder/MinorColor.java
+++ colorcoder/MinorColor.java
@@ -1,6 +1,6 @@
 package colorcoder;
 
-public enum MinorColor {
+public enum MinorColor implements  Color {
 
 	BLUE(0), ORANGE(1), GREEN(2), BROWN(3), SLATE(4);
 
@@ -10,17 +10,8 @@ public enum MinorColor {
 		this.index = index;
 	}
 
-	int getIndex() {
+	public int getIndex() {
 		return index;
-	}
-
-	public static MinorColor fromIndex(int index) {
-		for (MinorColor color : MinorColor.values()) {
-			if (color.getIndex() == index) {
-				return color;
-			}
-		}
-		return null;
 	}
 
 }
