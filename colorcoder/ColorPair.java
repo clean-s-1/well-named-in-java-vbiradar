75c1829911395e4fc6b6164df669e5590f245e80
vbiradar <vikhyat.biradar@gmail.com>
2022-05-27 12:41:36 +0530

    review changes

--- colorcoder/ColorPair.java
+++ colorcoder/ColorPair.java
@@ -32,16 +32,18 @@ public class ColorPair {
 		colorPairStr += minorColorNames[minorColor.getIndex()];
 		return colorPairStr;
 	}
-
+	
 	public ColorPair getColorFromPairNumber(int pairNumber) {
 		int zeroBasedPairNumber = pairNumber - 1;
 		MajorColor majorColor = ColorHelper.getIndexByEnum(MajorColor.class, zeroBasedPairNumber / numberOfMinorColors);
-		MinorColor minorColor = ColorHelper.getIndexByEnum(MinorColor.class, zeroBasedPairNumber % numberOfMinorColors);
+		MinorColor minorColor = ColorHelper.getIndexByEnum(MinorColor.class, zeroBasedPairNumber % numberOfMinorColors);;
+		//MajorColor majorColor = MajorColor.fromIndex(zeroBasedPairNumber / numberOfMinorColors);
+		//MinorColor minorColor = MinorColor.fromIndex(zeroBasedPairNumber % numberOfMinorColors);
 		return new ColorPair(majorColor, minorColor);
 	}
 
-	public int getPairNumberFromColor(MajorColor major, MinorColor minor) {
-		return major.getIndex() * numberOfMinorColors + minor.getIndex() + 1;
+	public int getPairNumberFromColor() {
+		return majorColor.getIndex() * numberOfMinorColors + minorColor.getIndex() + 1;
 	}
 
 }
